6960ec6185293fb47297e97517f71ac65e904912
1161645687@qq.com <dingjun2018>
2020-04-29 21:01:54 +0800

    同步第一版

---
+++ traffic_index/js/index.js
@@ -0,0 +1,31 @@
+$(function () {
+    layui.use(['form', 'laytpl', 'layer'], function () {
+        var form = layui.form,
+            laytpl = layui.laytpl,
+            layer = layui.layer;
+
+        var swiper = new Swiper('.swiper-container', {
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+
+        $(window).bind("scroll", function () {
+            console.log($(document).scrollTop());
+            console.log($(window).height());
+            if ($(document).scrollTop() > 500) {
+                $('.nav-fixed').show();
+                $('.kefu').show();
+            } else {
+                $('.nav-fixed').hide();
+                $('.kefu').hide();
+            }
+        });
+    });
+});
